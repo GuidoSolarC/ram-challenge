3c84240c8a8ed406d8a78cbb54ebf429c1b5f988
Guido Solar <guido.solar.c@gmail.com>
2021-05-04 10:15:27 -0400

    Se agrega font awesome, refactor de lógica, se agrega reload para cada componente

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -1,7 +1,13 @@
 <template>
     <!-- Card -->
     <div class="col-4">
-        <div class="card">
+        <div class="card shadow-sm">
+            <!-- Reload -->
+            <div class="card-header d-flex justify-content-end">                
+                <span v-on:click="reloadLocation()">    
+                    <i class="fas fa-sync"></i>
+                </span>    
+            </div>
             <div class="card-body">
                 <!-- Titulo -->
                 <h5 class="card-title">API Location: <i class="small">cuántas veces aparece la letra "l"</i></h5>
@@ -9,6 +15,8 @@
                 <p class="card-text">Cantidad: <strong>{{ this.cantidad }}</strong></p>
                 <!-- Mostrar tiempo de ejecución -->
                 <p class="card-text">Tiempo ejecución: <strong>{{ this.tiempo_ejecucion.toFixed(2) }}s ({{ this.tiempo_ejecucion.toFixed(4) }}ms)</strong></p>
+                <!-- Mensaje error -->
+                <p class="card-text small text-danger" v-if="error">Hubo un error en la ejecución.</p>
             </div>
         </div>
     </div>
@@ -19,35 +27,34 @@ export default {
 	methods: {
         // Consulto API
 		obtenerLocation: function () {
+            // Inicio "cronómetro" sólo en "page" inicial, y que no se sobreescriba el timer
+            if(this.pagina === 1){
+                this.tiempo_inicio = window.performance.now()
+            }            
             // Limpio array de location
             this.respuesta_location = []
-            // Declaro variables para performance
-            let tiempoInicio, tiempoTermino
-
-            // Inicio "cronómetro"
-            tiempoInicio = window.performance.now()
 
 			this.axios.get(this.apiLocation+'/?page=' + this.pagina, { 
             }).then(response => {
                 // Determino si la API devuelve más "pages" en "next"
-                this.next = response.data.info.next
+                this.next = response.data.info.next             
+                // Guardo la respuesta de los location
+                this.respuesta_location = response.data.results
+                // Recorro array y busco coincidencias
+                this.respuesta_location.forEach(element => {
+                    let coincidencia = (element.name.toLowerCase().match(/l/g) || []).length
+                    this.cantidad += coincidencia
+                })                         
                 // Continuo si "next" es distinto de null
-                if(this.next){                    
-                    // Guardo la respuesta de los location
-                    this.respuesta_location = response.data.results
-                    // Recorro array y busco coincidencias
-                    this.respuesta_location.forEach(element => {
-                        let coincidencia = (element.name.toLowerCase().match(/l/g) || []).length
-                        this.cantidad += coincidencia
-                    })                    
+                if(this.next){                      
                     // Seteo siguiente pagina y vuelvo a ejecutar API
                     this.pagina++
                     this.obtenerLocation()
                 } else {
                     // Termino "cronómetro"
-                    tiempoTermino = window.performance.now()                    
+                    this.tiempo_termino = window.performance.now()       
                     // Seteo valor en tiempo de ejecución
-                    this.tiempo_ejecucion = (tiempoTermino - tiempoInicio) / 1000
+                    this.tiempo_ejecucion = (this.tiempo_termino - this.tiempo_inicio) / 1000
                 }
             })
             .catch(error => {
@@ -58,11 +65,31 @@ export default {
                 this.next = null
                 this.pagina = 1
                 // Termino "cronómetro"
-                tiempoTermino = window.performance.now()
+                this.tiempo_termino = window.performance.now()
                 // Seteo valor en tiempo de ejecución
-                this.tiempo_ejecucion = (tiempoTermino - tiempoInicio) / 1000
+                this.tiempo_ejecucion = (this.tiempo_termino - this.tiempo_inicio) / 1000
+                // Error
+                this.error = true
             })
-		}
+		},
+        // Reseteo valores para volver a ejecutar API
+        limpiarValores: function(){
+            this.cantidad = 0
+            this.next = null
+            this.pagina = 1
+            this.tiempo_inicio = 0
+            this.tiempo_final = 0
+            this.tiempo_ejecucion = 0
+            this.error = false
+            this.respuesta_location = []
+        },
+        // Reload de API
+        reloadLocation: function(){
+            // Limpio valores
+            this.limpiarValores()
+            // Ejecuto API 
+            this.obtenerLocation()
+        }
 	},
 	mounted () {
         // Ejecuto la API una vez se construya el componente
@@ -77,8 +104,12 @@ export default {
             cantidad: 0,
             // Tiempo de ejecución
             tiempo_ejecucion: 0,
+            tiempo_inicio: 0,
+            tiempo_final: 0,
             // Pagina inicial de resultados
-            pagina: 1
+            pagina: 1,
+            // Error ejecucion
+            error: false
 		}
 	},
 	computed: {
--- src/main.js
+++ src/main.js
@@ -2,13 +2,22 @@ import Vue from 'vue'
 import App from './App'
 
 import store from './store'
+
+// https://router.vuejs.org
 import router from './router/index'
 
+// https://axios-http.com
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+// https://getbootstrap.com
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
+
+// https://fontawesome.com
+import "@fortawesome/fontawesome-free/js/brands"
+import "@fortawesome/fontawesome-free/js/solid"
+import "@fortawesome/fontawesome-free/js/fontawesome"
 
 Vue.config.productionTip = false
 
