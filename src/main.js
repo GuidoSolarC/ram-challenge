464b9764495718ec5c7a169349a63def275c95b4
Guido Solar <guido.solar.c@gmail.com>
2021-05-03 21:26:04 -0400

    Configuracion inicial de proyecto

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+
+import store from './store'
+import router from './router/index'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 Vue.config.productionTip = false
 
+Vue.use(VueAxios, axios)
+
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
